cab49f99e8e0b83e50cea6a1d5e5391883c41ec3
Diego Rojas <rojas_diego@outlook.com>
2022-09-01 19:03:28 -0300

    commit de prueba- por intelliJ

--- src/com.educacionit/clase1/Hello.java
+++ src/com.educacionit/clase1/Hello.java
@@ -2,6 +2,6 @@ package com.educacionit.clase1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hola mundo cruel");
+        System.out.println("Hola mundito");
     }
 }
